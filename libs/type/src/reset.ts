722c0f42b97aca0bf599879a5c1916e869605f56
xeho91 <xeho91@pm.me>
2024-06-20 18:58:02 +0800

    Initial code

---
+++ libs/type/src/reset.ts
@@ -0,0 +1,9 @@
+/**
+ * Some of the default types doesn't make sense or make it difficult to deal with.
+ *
+ * @see {@link https://github.com/total-typescript/ts-reset}
+ *
+ * @module reset
+ */
+
+import "@total-typescript/ts-/set";
