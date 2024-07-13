09d63f1883f8439dbe096e7104243cd560e71ccb
Mateusz Kadlubowski <xeho91@pm.me>
2024-07-13 21:40:26 +0800

    init

--- libs/storybook/scripts/build-types.ts
+++ libs/storybook/scripts/build-types.ts
@@ -2,13 +2,16 @@ import child_process from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 import url from "node:url";
+
 import { log } from "@xeho91/lib-logger";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-async function main() {
-	log.info("Running svelte-package...");
+async function build_types() {
+	log.info("Starting script `build:types`...");
+
+	log.info("Running svelte-package to build types...");
 	child_process.spawnSync("svelte-package", ["--input", "src/", "--types"]);
 
 	const output_dir = path.join(__dirname, "..", "dist");
@@ -20,15 +23,17 @@ async function main() {
 	for (const dirent of svelte_dts_files) {
 		const { name, parentPath } = dirent;
 		const src = path.join(parentPath, name);
-		const dest = path.join(parentPath.replace("dist", "src"), name);
+		const dest_dir = parentPath.replace("dist", "types");
+		if (!fs.existsSync(dest_dir)) fs.mkdirSync(dest_dir, { recursive: true });
+		const dest = path.join(dest_dir, name);
 		log.info(`Copying: ${url.pathToFileURL(src)}\nto: ${url.pathToFileURL(dest)}`);
 		fs.copyFileSync(src, dest);
 	}
 
 	log.info("Cleaning dist directory");
-	fs.rmdirSync(output_dir, { recursive: true });
+	fs.rmSync(output_dir, { recursive: true });
 
-	log.info("Finished!");
+	log.info("Finished script `build:types`!");
 }
 
-await main();
+await build_types();
