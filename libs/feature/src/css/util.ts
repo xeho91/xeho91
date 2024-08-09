1e477fbc3e7f2789d2b86db4cee21807f422c674
Mateusz Kadlubowski <xeho91@pm.me>
2024-08-09 15:22:46 +0800

    cleanup

--- libs/feature/src/css/util.ts
+++ libs/feature/src/css/util.ts
@@ -1,5 +1,5 @@
 import { SelectorClass } from "@xeho91/lib-css/selector/class";
-import { clsx } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import type { Action } from "svelte/action";
 
 export function merge_classes(...args: (Parameters<typeof clsx>[0] | SelectorClass)[]): string {
@@ -12,26 +12,24 @@ export function merge_classes(...args: (Parameters<typeof clsx>[0] | SelectorCla
 	return results;
 }
 
-type ClassInput = Parameters<typeof clsx>[0] | SelectorClass;
+type ClassInput = ClassValue | SelectorClass;
 
-export const classes: Action<Element, (ClassInput | ClassInput[])[]> = (node, classes) => {
+export const classes: Action<Element, ClassInput[]> = (node, ...classes) => {
 	// biome-ignore lint/style/useConst: Readability: It's mutating
 	let tokens: string[] = [];
-	if (Array.isArray(classes)) {
-		for (const selector_or_class of classes) {
-			if (selector_or_class instanceof SelectorClass) tokens.push(selector_or_class.name);
-			else if (selector_or_class) {
-				const names = clsx(selector_or_class).split(" ");
-				tokens.push(...names);
-			}
+	for (const selector_or_class of classes) {
+		if (selector_or_class instanceof SelectorClass) tokens.push(selector_or_class.name);
+		else if (selector_or_class) {
+			const names = clsx(selector_or_class).split(" ");
+			tokens.push(...names);
 		}
-	} else if (classes) tokens.push(classes.toString());
-	node.classList.add(...tokens);
-	return {
-		destroy() {
-			node.classList.remove(...tokens);
-		},
-	};
+		node.classList.add(...tokens);
+		return {
+			destroy() {
+				node.classList.remove(...tokens);
+			},
+		};
+	}
 };
 
 export interface WithClass {
