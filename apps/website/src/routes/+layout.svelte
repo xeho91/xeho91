801292abdeb34c3dd77f384314b303d4f38064fd
Mateusz Kadlubowski <xeho91@pm.me>
2024-08-14 17:40:03 +0800

    init website app root

---
+++ apps/website/src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+import "@xeho91/lib-asset/font/sans";
+import "@xeho91/lib-asset/font/serif";
+import "@xeho91/lib-asset/font/mono";
+
+import { Logo } from "@xeho91/lib-brand/logo";
+import { Color } from "@xeho91/lib-design/color";
+import { merge_classes } from "@xeho91/lib-feature/css";
+import { GlobalManagers } from "@xeho91/lib-feature/global";
+import type { Snippet } from "svelte";
+
+interface Props {
+	children: Snippet;
+}
+
+let { children }: Props = $props();
+</script>
+
+<svelte:head>
+	<meta
+		name="theme-color"
+		content={Color.get("secondary", "solid", 9).light_dark.light.oklch.toString()}
+		media="(prefers-color-scheme: light)"
+	/>
+	<meta
+		name="theme-color"
+		content={Color.get("secondary", "solid", 9).light_dark.dark.oklch.toString()}
+		media="(prefers-color-scheme: dark)"
+	/>
+</svelte:head>
+
+<GlobalManagers>
+	<main
+		class={merge_classes(
+			//
+			"flex flex-col",
+			"size-full",
+			"place-content-center",
+		)}
+	>
+		<header>
+			<Logo animated />
+		</header>
+
+		{@render children()}
+	</main>
+</GlobalManagers>
---
+++ apps/website/svelte.config.js
@@ -0,0 +1,29 @@
+import path from "node:path";
+import url from "node:url";
+
+import adapter from "@sveltejs/adapter-auto";
+import { CONFIG as SHARED_CONFIG } from "@xeho91/lib-config/svelte";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import("@sveltejs/kit").Config} */
+const CONFIG = {
+	...SHARED_CONFIG,
+	kit: {
+		adapter: adapter(),
+		alias: {
+			"$lib/*": "src/lib/*",
+		},
+		files: {
+			// TODO: Use lib-monorepo once transformed to JSDoc
+			assets: path.resolve(__dirname, "..", "..", "libs", "asset", "src"),
+			lib: path.resolve(__dirname, "src", "lib"),
+		},
+		prerender: {
+			handleHttpError: "warn",
+		},
+	},
+};
+
+export default CONFIG;
