c36fe696df3560457ceeb41ae3e8876ce74279da
Mateusz Kadlubowski <xeho91@pm.me>
2024-08-17 17:54:18 +0800

    refactor(ui): Improve `Skeleton` component (#48)

--- libs/ui/src/primitive/skeleton/Skeleton.svelte
+++ libs/ui/src/primitive/skeleton/Skeleton.svelte
@@ -2,59 +2,75 @@
 import { Color } from "@xeho91/lib-design/color";
 import { Radius } from "@xeho91/lib-design/radius";
 import { type WithClass, merge_classes } from "@xeho91/lib-feature/css";
+import { fade } from "svelte/transition";
 
 import type { SkeletonColor, SkeletonVariant } from "./util";
 
 interface Props extends WithClass {
 	color?: TColor;
+	background_color?: SkeletonColor;
 	count?: number;
 	variant?: SkeletonVariant;
+	hidden?: boolean;
 }
 
-const {
+let {
 	//
 	class: class_,
 	color = "gray" as TColor,
+	background_color = "gray",
 	count = 1,
+	hidden = false,
 	variant = "text",
 }: Props = $props();
+
+let rendered = $state(false);
+
+let color_1 = $derived(Color.get(color, "blend", 3));
+let color_2 = $derived(Color.get(color, "blend", 1));
+
+$effect(() => {
+	rendered = true;
+});
 </script>
 
-{#each { length: count } as _}
-	<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-	<span
+{#if !hidden}
+	<div
 		class={merge_classes(
 			"skeleton",
-			variant,
-			"outline-none relative overflow-hidden will-change-transform",
 			"inline-flex",
-			"w-full h-full min-w-[1em] min-h-[1em]",
-			"bg-no-repeat",
 			Color.class("background"),
-			Color.get(color, "blend", 5).class("background"),
+			Color.get(background_color, "solid", 3).class("background"),
 			variant === "circle" && Radius.get("circle").class(),
 			variant === "text" && Radius.get("m").class(),
 			class_,
 		)}
-		style:--color-1={Color.get(
-			color,
-			"blend",
-			3,
-		).light_dark.light.oklch.toString()}
-		style:--color-2={Color.get(
-			"gray",
-			"blend",
-			5,
-		).light_dark.light.oklch.toString()}
-		aria-busy="true"
-		aria-valuemin="0"
-		aria-valuemax="100"
-		aria-valuetext="Loading..."
-		role="progressbar"
-		tabindex="0"
+		style:--color-1={color_1.light_dark.light.atomized_oklch.toString()}
+		style:--color-2={color_2.light_dark.light.atomized_oklch.toString()}
+		in:fade|global={{ delay: 250, duration: rendered ? 250 : 0 }}
+		out:fade|global={{ delay: 250, duration: 250 }}
 	>
-	</span>
-{/each}
+		{#each { length: count } as _}
+			<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+			<span
+				class={merge_classes(
+					variant,
+					"outline-none relative overflow-hidden will-change-transform",
+					"inline-flex",
+					"w-full min-w-[1em] min-h-[1em]",
+					"bg-no-repeat",
+				)}
+				aria-busy="true"
+				aria-valuemin="0"
+				aria-valuemax="100"
+				aria-valuetext="Loading..."
+				role="progressbar"
+				tabindex="0"
+			>
+			</span>
+		{/each}
+	</div>
+{/if}
 
 <style>
 	@layer component {
