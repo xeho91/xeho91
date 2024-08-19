ad939466dede34fb98b53e30aa52f089fa20aef2
Mateusz Kadlubowski <xeho91@pm.me>
2024-08-19 21:56:06 +0800

    fix storybook layout order

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,9 +1,9 @@
+/* FIXME: Temporary workaround, to ensure the order stays as intended. */
+import "./global.css";
+
 import { withThemeByDataAttribute } from "@storybook/addon-themes";
 import type { Decorator, Parameters } from "@storybook/svelte";
 import { GlobalManagers } from "@xeho91/lib-feature/global";
-
-/*  FIXME: Temporary workaround, to ensure the order stays as intended. */
-import "./global.css";
 
 export const decorators: Decorator[] = [
 	withThemeByDataAttribute({
