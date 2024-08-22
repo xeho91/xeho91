e7c5bd944babc17efc551820acbcfa167b8225c1
Mateusz Kadlubowski <xeho91@pm.me>
2024-08-22 19:32:43 +0800

    update home route

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-import { merge_classes } from "@xeho91/lib-feature/css";
-import { Text } from "@xeho91/lib-ui/primitive/text";
-import { Heading } from "@xeho91/lib-ui/semantic/heading";
+import { PageHome } from "@xeho91/lib-ui/page/page-home";
 </script>
 
-<article class="size-full">
-	<section
-		class={merge_classes(
-			//
-			"size-full",
-			"flex flex-col place-content-center place-items-center",
-		)}
-	>
-		<Heading level={1} class="flex-col">
-			<Text size="5xl">{"🚧"}</Text>
-			<Text>{"Work in progress..."}</Text>
-		</Heading>
-	</section>
-</article>
+<PageHome />
