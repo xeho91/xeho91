c7571223f988f27fdbfb642e92b4ac38b39c12a2
Mateusz Kadlubowski <xeho91@pm.me>
2024-09-01 15:31:01 +0800

    init

---
+++ libs/feature/src/transition/mod.ts
@@ -0,0 +1,7 @@
+/**
+ * Custom transition functions for [Svelte transition](@link https://svelte.dev/docs/svelte-transition) directives.
+ *
+ * @module
+ */
+
+export { typewriter } from "./typewriter";
---
+++ libs/feature/src/transition/typewriter.ts
@@ -0,0 +1,22 @@
+import type { TransitionConfig } from "svelte/transition";
+
+interface TypewriterOptions {
+	speed?: number;
+}
+
+export function typewriter(element: Element, options: TypewriterOptions = {}): TransitionConfig {
+	const { speed = 1 } = options;
+	const child_node: Text | undefined = [...element.childNodes].find(
+		(n) => n.nodeType === Node.TEXT_NODE && n instanceof Text,
+	) as Text | undefined;
+	if (!child_node) throw new TypeError("This transition only works on elements with a single text node child");
+	const text = child_node.wholeText;
+	const duration = text.length / (speed * 0.01);
+	return {
+		duration,
+		tick: (t) => {
+			const index = Math.trunc(text.length * t);
+			element.textContent = text.slice(0, index);
+		},
+	};
+}
