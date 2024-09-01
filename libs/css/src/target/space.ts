fe75d4c98ddcbf7eb78cbc544e69b14a179c2a55
Mateusz Kadlubowski <xeho91@pm.me>
2024-09-01 15:25:10 +0800

    feat(css): Add `TargetScrollMargin` & `TargetScrollPadding`

--- libs/css/src/target/space.ts
+++ libs/css/src/target/space.ts
@@ -36,12 +36,12 @@ type TargetInset = IterableElement<typeof InsetTarget.TARGETS>;
 export class InsetTarget<TName extends TargetInset = TargetInset> implements ToProperty {
 	public static readonly TARGETS = readonly_set([
 		"inset",
-		"inset-inline",
-		"inset-inline-start",
-		"inset-inline-end",
 		"inset-block",
-		"inset-block-start",
 		"inset-block-end",
+		"inset-block-start",
+		"inset-inline",
+		"inset-inline-end",
+		"inset-inline-start",
 	]);
 
 	public static [Symbol.iterator](): IterableIterator<TargetInset> {
@@ -66,12 +66,12 @@ type TargetMargin = IterableElement<typeof MarginTarget.TARGETS>;
 export class MarginTarget<TName extends TargetMargin = TargetMargin> implements ToProperty {
 	public static readonly TARGETS = readonly_set([
 		"margin",
-		"margin-inline",
-		"margin-inline-start",
-		"margin-inline-end",
 		"margin-block",
 		"margin-block-start",
 		"margin-block-end",
+		"margin-inline",
+		"margin-inline-start",
+		"margin-inline-end",
 	]);
 
 	public static [Symbol.iterator](): IterableIterator<TargetMargin> {
@@ -79,6 +79,36 @@ export class MarginTarget<TName extends TargetMargin = TargetMargin> implements 
 	}
 
 	public static is_valid_name = (name: string): name is TargetMargin => MarginTarget.TARGETS.has(name);
+
+	public readonly name: TName;
+
+	constructor(name: TName) {
+		this.name = name;
+	}
+
+	public to_property(): Property<TName> {
+		const { name } = this;
+		return new Property(name);
+	}
+}
+
+type TargetScrollMargin = IterableElement<typeof ScrollMarginTarget.TARGETS>;
+export class ScrollMarginTarget<TName extends TargetScrollMargin = TargetScrollMargin> implements ToProperty {
+	public static readonly TARGETS = readonly_set([
+		"scroll-margin",
+		"scroll-margin-block",
+		"scroll-margin-block-start",
+		"scroll-margin-block-end",
+		"scroll-margin-inline",
+		"scroll-margin-inline-start",
+		"scroll-margin-inline-end",
+	]);
+
+	public static [Symbol.iterator](): IterableIterator<TargetScrollMargin> {
+		return ScrollMarginTarget.TARGETS[Symbol.iterator]();
+	}
+
+	public static is_valid_name = (name: string): name is TargetScrollMargin => ScrollMarginTarget.TARGETS.has(name);
 
 	public readonly name: TName;
 
@@ -122,8 +152,38 @@ export class PaddingTarget<TName extends TargetPadding = TargetPadding> implemen
 	}
 }
 
-type TargetWith = IterableElement<typeof WidthTarget.TARGETS>;
-export class WidthTarget<TName extends TargetWith = TargetWith> implements ToProperty {
+type TargetScrollPadding = IterableElement<typeof ScrollPaddingTarget.TARGETS>;
+export class ScrollPaddingTarget<TName extends TargetScrollPadding = TargetScrollPadding> implements ToProperty {
+	public static readonly TARGETS = readonly_set([
+		"scroll-padding",
+		"scroll-padding-inline",
+		"scroll-padding-inline-start",
+		"scroll-padding-inline-end",
+		"scroll-padding-block",
+		"scroll-padding-block-start",
+		"scroll-padding-block-end",
+	]);
+
+	public static [Symbol.iterator](): IterableIterator<TargetScrollPadding> {
+		return ScrollPaddingTarget.TARGETS[Symbol.iterator]();
+	}
+
+	public static is_valid_name = (name: string): name is TargetScrollPadding => ScrollPaddingTarget.TARGETS.has(name);
+
+	public readonly name: TName;
+
+	constructor(name: TName) {
+		this.name = name;
+	}
+
+	public to_property(): Property<TName> {
+		const { name } = this;
+		return new Property(name);
+	}
+}
+
+type TargetWidth = IterableElement<typeof WidthTarget.TARGETS>;
+export class WidthTarget<TName extends TargetWidth = TargetWidth> implements ToProperty {
 	public static readonly TARGETS = readonly_set([
 		//
 		"width",
@@ -131,11 +191,11 @@ export class WidthTarget<TName extends TargetWith = TargetWith> implements ToPro
 		"min-width",
 	]);
 
-	public static [Symbol.iterator](): IterableIterator<TargetWith> {
+	public static [Symbol.iterator](): IterableIterator<TargetWidth> {
 		return WidthTarget.TARGETS[Symbol.iterator]();
 	}
 
-	public static is_valid_name = (name: string): name is TargetWith => WidthTarget.TARGETS.has(name);
+	public static is_valid_name = (name: string): name is TargetWidth => WidthTarget.TARGETS.has(name);
 
 	public readonly name: TName;
 
@@ -190,7 +250,9 @@ export class SpaceTarget<TName extends Target = Target> implements ToProperty {
 		GapTarget.TARGETS,
 		InsetTarget.TARGETS,
 		MarginTarget.TARGETS,
+		ScrollMarginTarget.TARGETS,
 		PaddingTarget.TARGETS,
+		ScrollPaddingTarget.TARGETS,
 		WidthTarget.TARGETS,
 		HeightTarget.TARGETS,
 	);
@@ -217,7 +279,9 @@ export class SpaceTarget<TName extends Target = Target> implements ToProperty {
 				if (GapTarget.is_valid_name(name)) return new GapTarget(name).to_property() as TargetProperty<TName>;
 				if (InsetTarget.is_valid_name(name)) return new InsetTarget(name).to_property() as TargetProperty<TName>;
 				if (MarginTarget.is_valid_name(name)) return new MarginTarget(name).to_property() as TargetProperty<TName>;
+				if (ScrollMarginTarget.is_valid_name(name)) return new ScrollMarginTarget(name).to_property() as TargetProperty<TName>;
 				if (PaddingTarget.is_valid_name(name)) return new PaddingTarget(name).to_property() as TargetProperty<TName>;
+				if (ScrollPaddingTarget.is_valid_name(name)) return new ScrollPaddingTarget(name).to_property() as TargetProperty<TName>;
 				if (WidthTarget.is_valid_name(name)) return new WidthTarget(name).to_property() as TargetProperty<TName>;
 				if (HeightTarget.is_valid_name(name)) return new HeightTarget(name).to_property() as TargetProperty<TName>;
 				throw unrecognized(name);
@@ -234,10 +298,14 @@ type TargetProperty<TName extends Target> = TName extends "border-spacing" | "fl
 			? InferProperty<InsetTarget<TName>>
 			: TName extends TargetMargin
 				? InferProperty<MarginTarget<TName>>
-				: TName extends TargetPadding
-					? InferProperty<PaddingTarget<TName>>
-					: TName extends TargetWith
-						? InferProperty<WidthTarget<TName>>
-						: TName extends TargetHeight
-							? InferProperty<HeightTarget<TName>>
-							: never;
+				: TName extends TargetScrollMargin
+					? InferProperty<ScrollMarginTarget<TName>>
+					: TName extends TargetPadding
+						? InferProperty<PaddingTarget<TName>>
+						: TName extends TargetScrollPadding
+							? InferProperty<ScrollPaddingTarget<TName>>
+							: TName extends TargetWidth
+								? InferProperty<WidthTarget<TName>>
+								: TName extends TargetHeight
+									? InferProperty<HeightTarget<TName>>
+									: never;
