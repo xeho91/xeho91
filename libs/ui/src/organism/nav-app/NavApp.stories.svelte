7e894bc1ed209bc2a05b36eaf19aea8a21da1371
Mateusz Kadlubowski <xeho91@pm.me>
2024-09-14 22:06:48 +0800

    fix biome

--- libs/ui/src/organism/nav-app/NavApp.stories.svelte
+++ libs/ui/src/organism/nav-app/NavApp.stories.svelte
@@ -1,43 +1,43 @@
 <script lang="ts" context="module">
-	import { action } from "@storybook/addon-actions";
-	import { defineMeta } from "@storybook/addon-svelte-csf";
-	import { SHARED_META } from "@xeho91/lib-storybook/meta";
-	import { PARAMETERS } from "@xeho91/lib-storybook/parameters";
-	import { SvelteURL } from "svelte/reactivity";
+import { action } from "@storybook/addon-actions";
+import { defineMeta } from "@storybook/addon-svelte-csf";
+import { SHARED_META } from "@xeho91/lib-storybook/meta";
+import { PARAMETERS } from "@xeho91/lib-storybook/parameters";
+import { SvelteURL } from "svelte/reactivity";
 
-	import NavApp from "./NavApp.svelte";
+import NavApp from "./NavApp.svelte";
 
-	import { Container } from "#layout/default/mod";
+import { Container } from "#layout/default/mod";
 
-	let current = $state(new SvelteURL(window.location.toString()));
+let current = $state(new SvelteURL(window.location.toString()));
 
-	current.pathname = "/";
+current.pathname = "/";
 
-	const { Story } = defineMeta({
-		...SHARED_META,
-		component: NavApp,
-		title: "Organism/NavApp",
-		decorators: [
-			() => ({
-				// @ts-expect-error FIXME: Need to fix addon
-				Component: Container,
-			}),
-		],
-		parameters: {
-			layout: "centered",
-			sveltekit_experimental: {
-				hrefs: {
-					"/*": {
-						callback: (to: string) => {
-							current.pathname = to;
-							action("onNavigate")(to);
-						},
-						asRegex: true,
+const { Story } = defineMeta({
+	...SHARED_META,
+	component: NavApp,
+	title: "Organism/NavApp",
+	decorators: [
+		() => ({
+			// @ts-expect-error FIXME: Need to fix addon
+			Component: Container,
+		}),
+	],
+	parameters: {
+		layout: "centered",
+		sveltekit_experimental: {
+			hrefs: {
+				"/*": {
+					callback: (to: string) => {
+						current.pathname = to;
+						action("onNavigate")(to);
 					},
+					asRegex: true,
 				},
 			},
 		},
-	});
+	},
+});
 </script>
 
 <Story
